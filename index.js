8678113a88e47073f37629a77e17c84db0d4f7bc
Gideon Ogot Junior <juniorgideon99@gmail.com>
2023-12-13 15:04:11 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 
-// Write your code here
+var num1 = 31;
+var num2 = 2;
+
+var multiply = num1 * num2;
+
+console.log(multiply)
+
+
+
+
+var random= Math.ceil(Math.random() * 9) ;
+console.log(random)
+
+
+
+var num3=12;
+var num4=8;
+
+var mod = num3 %  num4;
+ console.log(mod)
+
+
+ 
+   var max=Math.max(5,10,15,20)
+
+   console.log(max)
